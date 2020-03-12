3e08db44c25ecc0daac777c01f5fcf7f6c9b16c5
Javier Sanchez <xtrs84zk@gmail.com>
2020-03-12 17:00:12 -0600

    7) Computed properties

--- main.js
+++ main.js
@@ -6,23 +6,27 @@ var app = new Vue({
     data: { //En esta parte va la información cruda.
         brand: 'Vue Mastery',
         product: 'Socks',
+        selectedVariant: 0,
         description: "It's a sock",
-        image: './assets/image.png',
         link: 'xtrs84zk.now.sh',
-        inStock: true,
-        noStockClass: 'text-decoration: line-through',
+        onSale: true,
+        objectStyle: { //apply styles with objects
+            textDecoration: 'line-through'
+        },
         inventory: 100,
         details: ["80% cotton", "20% polyester", "Gender-neutral"],
         variants: [
             {
                 variantId: 2234,
                 variantColor: "green",
-                variantImage: "./assets/image.png"
+                variantImage: "./assets/image.png",
+                variantQuantity: 10
             },
             {
                 variantId: 2235,
                 variantColor: "blue",
-                variantImage: "./assets/image-blue.png"
+                variantImage: "./assets/image-blue.png",
+                variantQuantity: 0
             }
 
         ],
@@ -43,14 +47,30 @@ var app = new Vue({
         ],
     },
     methods: { //Aquí van los métodos; la notación usada podría no funcinar en todos los navegadores, pero... funciona en Firefox y Safari.
-        updateProduct(variantImage){
-            this.image = variantImage;
+        updateProduct(index){
+            this.selectedVariant = index;
         },
         addToCart(){
             this.cart += 1;
         },
         unAddToCart(){
             this.cart -= 1;
+        }
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product
+        },
+        image() {
+            return this.variants[this.selectedVariant].variantImage
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].variantQuantity
+        },
+        itemOnSale(){
+            if(this.onSale){
+                return 'From the ' + this.brand  + ' brand, the item ' + this.product +  ' is on sale' 
+            }
         }
     }
 })
